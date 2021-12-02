6fefbe552c1e8557dcf542349a6a383c475065d7
angelriverarr <87913638+angelriverarr@users.noreply.github.com>
2021-12-02 19:48:31 -0300

    new

--- PryectoMovieTheater/Models/UpData.cs
+++ PryectoMovieTheater/Models/UpData.cs
@@ -20,6 +20,7 @@ namespace PryectoMovieTheater.Models
         public string Ubicacion { get; set; }
         public string Caratula { get; set; }
         public string Genero { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> Año { get; set; }
         public string Sinopsis { get; set; }
         public HttpPostedFileBase file { get; set; }
