ee564142fccdcf6b2f48a66fd584cfd8a9d8736c
Enrique-RAMOS <84140676+Enrique-RAMOS@users.noreply.github.com>
2021-12-02 16:54:58 -0600

    new

--- PryectoMovieTheater/Models/UpData.cs
+++ PryectoMovieTheater/Models/UpData.cs
@@ -11,6 +11,7 @@ namespace PryectoMovieTheater.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Web;
 
     public partial class UpData
